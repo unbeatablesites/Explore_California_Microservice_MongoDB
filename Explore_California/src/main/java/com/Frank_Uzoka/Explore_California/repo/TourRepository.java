8dba53b92c27bdc9fc558f8e7adf9a82f2be074d
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-18 15:45:42 -0500

    Adding class

---
+++ Explore_California/src/main/java/com/Frank_Uzoka/Explore_California/repo/TourRepository.java
@@ -0,0 +1,8 @@
+package com.Frank_Uzoka.Explore_California.repo;
+
+import com.Frank_Uzoka.Explore_California.domain.Tour;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+}
+
