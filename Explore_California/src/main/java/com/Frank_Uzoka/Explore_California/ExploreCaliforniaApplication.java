24bc8ea9b86f697fc145f9ac3c6c995e074f5678
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-18 14:04:09 -0500

    Adding dependencies

--- Explore_California/src/main/java/com/Frank_Uzoka/Explore_California/ExploreCaliforniaApplication.java
+++ Explore_California/src/main/java/com/Frank_Uzoka/Explore_California/ExploreCaliforniaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExploreCaliforniaApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(ExploreCaliforniaApplication.class, args);
 	}
